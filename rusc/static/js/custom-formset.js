46c184dcffb3097c44919b7fedbed8a9bacadd18
Zartch <zartch@gmail.com>
2015-06-22 23:22:10 +0200

    Separat el javascrip per controlar el formset de recurs que es cridat a la creació de posts

---
+++ rusc/static/js/custom-formset.js
@@ -0,0 +1,12 @@
+
+$( document ).ready(function() {
+
+
+        $('#MyForm tbody tr').formset({
+            prefix: 'form',
+            addText: 'afegir recurs',
+            deleteText: 'remove'
+        });
+
+
+ });
