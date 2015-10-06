debf3940b5ba413971c54acab9be2b4c9d829e0b
Zartch <zartch@gmail.com>
2015-10-06 17:35:45 +0200

    autocomplete_light substituit per Select2

--- recurs/forms.py
+++ recurs/forms.py
@@ -1,10 +1,6 @@
 from django import forms
-import autocomplete_light
 from etiqueta.models import Etiqueta
-from recurs.models import Recurs
 from cela.models import get_cela
-
-autocomplete_light.register(Etiqueta, search_fields=('nom', ))
 
 class RecursForm(forms.Form):
     """
@@ -15,22 +11,37 @@ class RecursForm(forms.Form):
                     widget=forms.TextInput(attrs={
                         'placeholder': 'Recurs Name',
                     }),
-                    required=False)
+                    required=True)
     url = forms.CharField(
                     max_length=100,
                     widget=forms.TextInput(attrs={
                         'placeholder': 'URL',
                     }),
                     required=False)
-    etiquetes =  autocomplete_light.MultipleChoiceField('EtiquetaAutocomplete')
-
+    #etiquetes =  autocomplete_light.MultipleChoiceField('EtiquetaAutocomplete')
+    etiquetes = forms.ModelChoiceField(queryset="", initial="")
     adjunt = forms.FileField()
 
 
+    def clean(self):
+        super(RecursForm, self).clean() #if necessary
+        if 'etiquetes' in self._errors:
+            del self._errors['etiquetes']
+        if self.data['adjunt']=="" and  self.data['url']== "":
+            self._errors['url'] = ["Es necesari un adjunt o una URL"]
+            self._errors['adjunt'] = ["Es necesari un adjunt o una URL"]
+        return self.cleaned_data
 
     def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request')
         super(RecursForm, self).__init__(*args, **kwargs)
         self.fields['etiquetes'].required = False
         self.fields['descripcio'].required = False
+        self.fields['url'].widget.attrs['class'] = 'form-control'
+        self.fields['descripcio'].widget.attrs['class'] = 'form-control'
         self.fields['adjunt'] = forms.FileField(u'imagen', widget=forms.ClearableFileInput(attrs={'class':'btn btn-md btn-primary'}))
         self.fields['adjunt'].required = False
+        self.fields['etiquetes'].queryset = Etiqueta.objects.filter(cela= get_cela(self.request))
+        self.fields['etiquetes'].widget.attrs['class'] = 'etiquetes'
+        self.fields['etiquetes'].widget.attrs['style'] = 'width: 100%'
+        self.fields['etiquetes'].widget.attrs['multiple'] = 'multiple'
--- recurs/views.py
+++ recurs/views.py
@@ -44,19 +44,32 @@ def recursCreateView(request):
             return redirect('auth_login')
     cela= get_cela(request)
     #https://docs.djangoproject.com/en/1.8/topics/http/file-uploads/
-    formRecurs = RecursForm(request.POST or None)
+    formRecurs = RecursForm(request.POST or None, request= request)
+
+
 
     if formRecurs.is_valid():
         data = formRecurs.cleaned_data
         #handle_uploaded_file(request.FILES['file'])
-        rec = Recurs.objects.create(descripcio=data['descripcio'],
-                                    url=data['url'],
-                                    cela= cela,
-                                    autor=request.user,
-                                    adjunt=request.FILES['adjunt'])
+
+        from django.utils.datastructures import MultiValueDictKeyError
+
+        try:
+            aj = request.FILES['adjunt']
+            rec = Recurs.objects.create(descripcio=data['descripcio'],
+                                        url=data['url'],
+                                        cela= cela,
+                                        autor=request.user,
+                                        adjunt=request.FILES['adjunt'])
+        except MultiValueDictKeyError:
+            rec = Recurs.objects.create(descripcio=data['descripcio'],
+                                        url=data['url'],
+                                        cela= cela,
+                                        autor=request.user)
+
 
         #Recollim les variables per a crear les etiquetes  asociales al recurs
-        etqAdd = request.POST.get('etiquetes-autocomplete', 0)
+        #etqAdd = request.POST.get('etiquetes-autocomplete', 0)
         etqlist = request.POST.getlist('etiquetes', 0)
 
 
@@ -65,21 +78,24 @@ def recursCreateView(request):
             if len(etqlist) > 0:
                 #Los tags que ya estaban en DB
                 for etiqueta in etqlist:
-                    objEtq = Etiqueta.objects.filter(pk=etiqueta).first()
+                    try:
+                        objEtq = Etiqueta.objects.filter(pk=etiqueta,cela=cela).first()
+                    except ValueError:
+                    #creem etiqueta y la afegim al recurs
+                        objEtq = Etiqueta.objects.create(nom= etiqueta,cela=cela)
+
                     rec.etiquetes.add(objEtq)
 
-        if len(etqAdd) > 0:
-            etqAdd = str.split(etqAdd, ",")
-            #Los tags que se tienen que añadir
-            for etiqueta in etqAdd:
-                objEtq, created = Etiqueta.objects.get_or_create(nom=etiqueta.strip(), cela=cela)
-                rec.etiquetes.add(objEtq)
+
+        # if len(etqAdd) > 0:
+        #     etqAdd = str.split(etqAdd, ",")
+        #     #Los tags que se tienen que añadir
+        #     for etiqueta in etqAdd:
+        #         objEtq, created = Etiqueta.objects.get_or_create(nom=etiqueta.strip(), cela=cela)
+        #         rec.etiquetes.add(objEtq)
 
 
         notif_messages.add_message(request, notif_messages.INFO, "Has creat un nou recurs", 'success')
-
-
-
 
 
     return render(request, 'recurs/recurs_form.html',
