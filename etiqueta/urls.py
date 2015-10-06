e33a8aab653d41b0a16e33e64d91454bcefaaae5
Zartch <zartch@gmail.com>
2015-10-06 21:02:00 +0200

    Reecolocació de las URL per tal que fuincionin com haurien de funcionar

--- etiqueta/urls.py
+++ etiqueta/urls.py
@@ -6,10 +6,10 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
         url(r'^(?P<etq>\d+)/$', views.etiquetaview,name='etiqueta'),
+        url(r'^modtesauro/(?P<pk>\d+)/$', views.tesauroUpdateView.as_view() ,name='mod_tesauro'),
         url(r'^(?P<nometq>[\w\s]+)/(?P<nomcela>[\w\s]+)/$', views.nometiquetaview,name='nom_etiqueta'),
         url(r'^todo/(?P<pkpost>\d+)$', views.feinafeta, name='feina_feta'),
         url(r'^todo', views.todoview, name='todo'),
         url(r'^tesauronou/$', login_required(views.tesauroCreateView.as_view()) ,name='tesauro_nou'),
-        url(r'^modtesauro/(?P<pk>\d+)/$', views.tesauroUpdateView.as_view() ,name='mod_tesauro'),
         url(r'^deltesauro/(?P<pk>\d+)/$', views.tesauroDeleteView.as_view() ,name='delete_tesauro'),
 )
