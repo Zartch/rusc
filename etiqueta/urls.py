11bcd340dd69efe62c5afb9bebeb9a137f09fd29
Zartch <zartch@gmail.com>
2015-10-15 17:16:07 +0200

    Afegits decorators per que demani login

--- etiqueta/urls.py
+++ etiqueta/urls.py
@@ -5,9 +5,9 @@ from etiqueta import views
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
-        url(r'^(?P<etq>\d+)/$', views.etiquetaview,name='etiqueta'),
+        url(r'^(?P<etq>\d+)/$', login_required(views.etiquetaview),name='etiqueta'),
         url(r'^modtesauro/(?P<pk>\d+)/$', views.tesauroUpdateView.as_view() ,name='mod_tesauro'),
-        url(r'^(?P<nometq>[-\w]+)/(?P<nomcela>[-\w]+)/$', views.nometiquetaview,name='nom_etiqueta'),
+        url(r'^(?P<nometq>[-\w]+)/(?P<nomcela>[-\w]+)/$', login_required(views.nometiquetaview),name='nom_etiqueta'),
         url(r'^todo/(?P<pkpost>\d+)$', views.feinafeta, name='feina_feta'),
         url(r'^todo', views.todoview, name='todo'),
         url(r'^tesauronou/$', login_required(views.tesauroCreateView.as_view()) ,name='tesauro_nou'),
